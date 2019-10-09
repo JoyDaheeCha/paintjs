d04138a92bf75756604cacab634cbae38e9ccd8b
JoyDaheeCha <56214102+JoyDaheeCha@users.noreply.github.com>
2019-10-09 18:47:46 +0900

    change brush color

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 500;
+canvas.height = 500;
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,23 +13,32 @@ function stopPainting(event) {
     painting = false;
 }
 
-function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
